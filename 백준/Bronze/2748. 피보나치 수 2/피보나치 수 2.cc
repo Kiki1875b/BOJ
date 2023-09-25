76f85e82c73e8af9bb7efa9bec9511edd8eb6c51
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2023-09-25 21:46:52 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[91];
+int main(){
+    dp[0] = 0;
+    dp[1] = 1;
+    int n;
+    cin >> n;
+    for(int i = 2; i<=n; i++){
+        dp[i] = dp[i-2] + dp[i - 1];
+    }
+    cout << dp[n];
+
+}
