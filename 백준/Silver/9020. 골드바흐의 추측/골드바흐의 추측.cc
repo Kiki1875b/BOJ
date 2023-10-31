106b2cf71ee185166551496ae06b4f62ea81362c
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2023-10-31 21:13:38 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 212 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+int t, n;
+bool arr2[10001];
+
+
+
+int main(){
+
+    fill(arr2, arr2 + 10000, true);
+    for(int i = 2; i<=floor(sqrt(10000))+1; i++){
+        for(int j = i*i; j<= 10000; j += i){
+            arr2[j] = false;
+        }
+    }
+
+    arr2[0] = false; arr2[1] = false;
+
+    cin >> t;
+
+    while(t--){
+        cin >> n;
+
+        int m = floor(n/2);
+
+        for(int i = m; i >= 2; i--){
+            if(arr2[i] && arr2[n-i]){
+                cout << i << " " << n - i << "\n";
+                break;
+            }
+        }
+    }
+}
