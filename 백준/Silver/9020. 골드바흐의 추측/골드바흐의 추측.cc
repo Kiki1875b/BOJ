bf5f18ab25bcc666ef61d47134f4a67704238a7f
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2023-10-31 21:26:25 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -9,7 +9,8 @@ bool arr2[10001];
 
 
 int main(){
-
+    ios::sync_with_stdio(false);
+	cin.tie(0);
     fill(arr2, arr2 + 10000, true);
     for(int i = 2; i<=floor(sqrt(10000))+1; i++){
         for(int j = i*i; j<= 10000; j += i){
