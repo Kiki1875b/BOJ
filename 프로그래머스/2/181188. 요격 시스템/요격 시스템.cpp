e6fd280af7660f5dd6da4f6ce2e34366df833357
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2024-08-21 16:32:32 +0900

    [level 2] Title: 요격 시스템, Time: 191.18 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -1,29 +1,28 @@
-//
-// Created by User on 2023-12-29.
-// 요격 시스템
-#include <string>
-#include <vector>
-#include <algorithm>
 
+#include <bits/stdc++.h>
 using namespace std;
 
 
-bool cmp(vector<int> &t1, vector<int> &t2){
+bool cmp(vector<int>& t1, vector<int>& t2){
     return t1[1] < t2[1];
 }
 
 int solution(vector<vector<int>> targets) {
     int answer = 0;
     int last = -1;
+
     sort(targets.begin(), targets.end(), cmp);
 
-    for(auto i: targets){
-        if(i[0] < last && i[1] >= last) continue;
-        else{
+    for(auto t : targets){
+        if(t[0] < last && t[1] >= last){
+            continue;
+        }else{
+            last = t[1];
             answer++;
-            last = i[1];
         }
     }
+
+
 
     return answer;
 }
