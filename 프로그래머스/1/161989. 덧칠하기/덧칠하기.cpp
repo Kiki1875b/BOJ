d9a5679e35da8bc4973ca1ec9d674979ae9f204d
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2024-08-28 12:23:18 +0900

    [level 1] Title: 덧칠하기, Time: 0.23 ms, Memory: 5.77 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int last = 0;
+    
+    for(int i = 0; i<section.size(); i++){
+        if(section[i] > last){
+            answer++;
+            last = section[i] + m -1;
+        }
+    }
+
+    return answer;
+}
