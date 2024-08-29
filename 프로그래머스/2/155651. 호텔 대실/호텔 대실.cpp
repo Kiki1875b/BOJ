9d7cf6c30753e996c4d733651efbc17aa4017274
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2024-08-29 15:01:40 +0900

    [level 2] Title: 호텔 대실, Time: 1.73 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -1,73 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int,int>> timeToInt(vector<vector<string>> time){
+
+
+vector<pair<int,int>> timeToInt(vector<vector<string>>& time){
     vector<pair<int,int>> ret;
     for(int i = 0; i<time.size(); i++){
-        vector<string> t = time[i];
-        pair<int,int> temp = {0,0};
 
-        int hour, minutes;
-        char colon;
+        pair<int,int> temp {0,0};
+        int hour, minute;
+        char col;
 
-        istringstream timeStart(time[i][0]);
-        timeStart >> hour >> colon >> minutes;
-        temp.first = hour*60+minutes;
+        istringstream start(time[i][0]);
+        start >> hour >> col >> minute;
+        temp.first = hour * 60 + minute;
 
-        istringstream timeEnd(time[i][1]);
-        timeEnd >> hour >> colon >> minutes;
-        temp.second = hour*60 + minutes + 10;
+        istringstream end(time[i][1]);
+        end >> hour >> col >> minute;
+        temp.second = hour * 60 + minute + 10;
 
         ret.push_back(temp);
     }
     return ret;
 }
 
-
 bool isPossible(int num, vector<pair<int,int>>& time){
-    int maxCnt = 0;
-    int cnt = 0;
 
-    std::sort(time.begin(), time.end(), [](pair<int,int> a, pair<int,int> b){
-        return a.first < b.first;
-    });
     priority_queue<int, vector<int>, greater<int>> pq;
 
-    for(auto interval : time){
+    int cnt = 0, maxCnt = 0;
 
-        while(!pq.empty() && pq.top() <= interval.first){
+    for(auto t : time){
+        while(!pq.empty() && t.first >= pq.top()){
             pq.pop();
             cnt--;
         }
 
-        pq.push(interval.second);
+        pq.push(t.second);
         cnt++;
-
         maxCnt = max(cnt, maxCnt);
-        if (pq.size() > num) return false;
+        if(pq.size() > num) return false;
     }
-
     return true;
 }
 
-
 int solution(vector<vector<string>> book_time) {
     int answer = INT_MAX;
+
     vector<pair<int,int>> time = timeToInt(book_time);
 
-    int l = 0;
-    int r = 99999999;
+    sort(time.begin(), time.end(), [](pair<int,int> a, pair<int,int> b){
+        return a.first < b.first;
+    });
+
+    int l = 0; int r = 1001;
 
     while(l<=r){
         int mid = (l+r)/2;
         if(isPossible(mid, time)){
+            answer = min(mid,answer);
             r = mid - 1;
-            answer = min(answer, mid);
-        }else{
-            l = mid + 1;
-        }
+        }else l = mid + 1;
     }
 
 
+
     return answer;
+
 }
