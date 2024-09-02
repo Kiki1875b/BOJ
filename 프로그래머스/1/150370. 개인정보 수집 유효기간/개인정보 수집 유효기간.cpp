9d0875d3cf79c3381eebffdfbeca19b200606249
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2024-09-02 12:21:01 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.12 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -1,64 +1,46 @@
-#include <string>
-#include <map>
-#include <vector>
-#include <sstream>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 
-int stringToInt(string temp){
+int dateToInt(string date){
+    int year, month, day;
+    char delimiter;
+    istringstream d(date);
 
-    istringstream iss(temp);
-    string token;
-    int totalHours = 0;
-    int year = 0, month = 0, day = 0;
+    d >> year >> delimiter >> month >> delimiter >> day;
 
-    while(getline(iss, token, '.')){
-        int value = stoi(token);
-        if(year == 0) year = value;
-        else if(month == 0) month = value;
-        else if(day == 0) day = value;
-    }
-
-    totalHours = (year*12*28 + (month-1)*28 + day);
-    return totalHours;
-
+    return (year * 12 * 28) + ((month - 1) * 28) + day;
 }
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
     vector<int> answer;
-    map<char, int> m1;
-    map<char, int> m2;
-    int curDay = stringToInt(today);
+    unordered_map<char, int> t;
+    unordered_map<char, vector<int>> p_map;
 
 
-    for (int i = 0; i < terms.size(); i++) {
-        istringstream iss(terms[i]);
-        string token;
-        string month = terms[i].substr(2);
-        while (iss >> token) {
+    int curDate = dateToInt(today);
 
-            m1[token[0]] = stoi(month);
-        }
+    for(auto temp : terms){
+        t[temp[0]] = stoi(temp.substr(2)) * 28;
     }
 
-    for(int i = 0; i<privacies.size(); i++){
-        int y = stoi(privacies[i].substr(0, 4));
-        int m = stoi(privacies[i].substr(5, 2));
-        int d = stoi(privacies[i].substr(8, 2));
-        char a = privacies[i].back();
+    int cnt = 1;
 
-        int tmp = y * 12 * 28 + (m - 1) * 28 + d + (m1[a]*28 - 1);
+    for(auto priv: privacies){
+        string num;
+        char c;
+        istringstream temp(priv);
+        temp >> num >> c;
 
-        if(tmp < curDay){
-            answer.emplace_back(i+1);
+        int startDay = dateToInt(num);
+        int days = t[c];
+
+        if(startDay + days -1 < curDate){
+            answer.push_back(cnt);
         }
-
+        cnt++;
     }
-
-
-
 
     return answer;
 }
+
