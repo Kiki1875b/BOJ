2f54f63fdb4c144d293c8299329b44371ea8817e
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-02-26 22:59:48 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 168 ms, Memory: 19448 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = br.readLine();
+            if (line.equals(".")) break;
+
+            sb.append(isBalanced(line) ? "yes\n" : "no\n");
+        }
+        
+        System.out.print(sb);
+    }
+
+    private static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
