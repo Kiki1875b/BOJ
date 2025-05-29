5f49e2ad8785b2178c8ec9bc8833503ea96951d7
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-05-29 12:00:36 +0900

    [level 2] Title: 무인도 여행, Time: 11.33 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,65 +1,79 @@
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Stack;
+
+class Pair {
+  int x;
+  int y;
+
+  public Pair(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
 
 class Solution {
+  boolean[][] visited;
+  int[] dx = new int[]{-1,1,0,0};
+  int[] dy = new int[]{0,0,1,-1};
+  public int[] solution(String[] maps) {
+    int[] answer;
 
-    int[] dx = {-1,1,0,0};
-    int[] dy = {0,0,-1,1};
-    int width, height;
+    visited = new boolean[maps.length][maps[0].length()];
 
-    boolean[][] visited;
-
-    public int bfs(int sx, int sy, String[] maps){
-        Queue<int[]> q = new LinkedList<>();
-        int temp = maps[sx].charAt(sy);
-        q.add(new int[]{sx, sy, maps[sx].charAt(sy) - '0'});
-        visited[sx][sy] = true;
-        int total = 0;
-        while(!q.isEmpty()){
-            int[] current = q.poll();
-            int x = current[0];
-            int y = current[1];
-            int cost = current[2];
-
-            total += cost;
-
-            for(int i = 0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= height || ny >= width) continue;
-                if(maps[nx].charAt(ny) == 'X') continue;
-                if(visited[nx][ny]) continue;
-
-                visited[nx][ny] = true;
-                q.add(new int[]{nx, ny, maps[nx].charAt(ny) - '0'});
-            }
+    List<Integer> tmp = new ArrayList<>();
+    for(int i = 0; i<maps.length; i++){
+      for(int j = 0; j<maps[i].length(); j++){
+        if(!visited[i][j] && maps[i].charAt(j) != 'X'){
+          int size = bfs(i, j, maps);
+          tmp.add(size);
         }
-        return total;
+      }
     }
-    public int[] solution(String[] maps) {
-
-
-        height = maps.length;
-        width = maps[0].length();
-        visited = new boolean[height][width];
-
-        List<Integer> sizeList = new ArrayList<>();
-        for(int i = 0; i<height; i++){
-            for(int j = 0; j<width; j++){
-                if(!visited[i][j] && maps[i].charAt(j) != 'X'){
-                    int tmp = bfs(i,j, maps);
-                    sizeList.add(tmp);
-                }
-            }
-        }
-
-        Collections.sort(sizeList);
-        int[] answer = new int[sizeList.size()];
-
-        for(int i = 0; i<sizeList.size(); i++){
-            answer[i] = sizeList.get(i);
-        }
-
-        return sizeList.isEmpty() ? new int[]{-1} : answer;
+    if(tmp.isEmpty()) return new int[]{-1};
+    Collections.sort(tmp);
+    answer = new int[tmp.size()];
+    for(int i = 0; i<tmp.size(); i++){
+      answer[i] = tmp.get(i);
     }
+
+    return answer;
+  }
+
+
+  int bfs(int x, int y, String[] maps){
+
+    Queue<Pair> q = new LinkedList<>();
+    q.add(new Pair(x, y));
+    visited[x][y] = true;
+    int ret = Integer.parseInt(String.valueOf(maps[x].charAt(y)));
+
+    while(!q.isEmpty()){
+      Pair cur = q.poll();
+      int cx = cur.x;
+      int cy = cur.y;
+
+      for(int i = 0; i<4; i++ ){
+        int nx = cx + dx[i];
+        int ny = cy + dy[i];
+
+        if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length()) continue;
+        if(maps[nx].charAt(ny) == 'X') continue;
+        if(!visited[nx][ny]){
+          visited[nx][ny] = true;
+          q.add(new Pair(nx, ny));
+          ret += Integer.parseInt(String.valueOf(maps[nx].charAt(ny)));
+        }
+      }
+    }
+
+    return ret;
+  }
 }
