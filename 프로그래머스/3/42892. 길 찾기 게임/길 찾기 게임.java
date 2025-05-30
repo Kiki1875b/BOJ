adf970b1e6204bab157ad2954e3a519bd206e144
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-05-30 15:40:02 +0900

    [level 3] Title: 길 찾기 게임, Time: 37.19 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,63 +1,60 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 
 class Node{
-  int level, x, num;
-  Node left, right;
-  
-  public Node(int level, int x, int num){
+  int level;
+  int x;
+  int nodeNum;
+  Node left;
+  Node right;
+
+  public Node(int level, int x, int nodeNum) {
     this.level = level;
     this.x = x;
-    this.num = num;
+    this.nodeNum = nodeNum;
   }
 }
 class Solution {
 
-  List<Integer> preorderList = new ArrayList<>();
-  List<Integer> postorderList = new ArrayList<>();
+  List<Integer> preorderResult = new ArrayList<>();
+  List<Integer> postorderResult = new ArrayList<>();
   public int[][] solution(int[][] nodeinfo) {
+//    int[][] answer = {};
 
-    int n = nodeinfo.length;
     List<Node> nodes = new ArrayList<>();
-    for(int i = 0; i<n; i++){
-      nodes.add(
-          new Node(nodeinfo[i][1], nodeinfo[i][0], i + 1)
-      );
+    for(int i = 0; i<nodeinfo.length; i++){
+      nodes.add(new Node(nodeinfo[i][1], nodeinfo[i][0], i + 1));
     }
-    
+
+
     nodes.sort((a, b) -> {
-      if(a.level != b.level) return b.level - a.level;
+      if(b.level != a. level) return b.level - a.level;
       else return a.x - b.x;
     });
-    
+
     Node root = nodes.get(0);
-    
-    for(int i = 1; i<n; i++){
+
+    for(int i = 1; i<nodeinfo.length; i++){
       insert(root, nodes.get(i));
     }
-    
-    preorder(root); postorder(root);
-    
-    int[][] ans = new int[2][n];
-    
-    for(int i = 0; i<n; i++){
-      ans[0][i] = preorderList.get(i);
-      ans[1][i] = postorderList.get(i);
+
+    preorder(root);
+    postorder(root);
+
+    int[][] answer = new int[2][nodeinfo.length];
+    for (int i = 0; i < nodeinfo.length; i++) {
+      answer[0][i] = preorderResult.get(i);
+      answer[1][i] = postorderResult.get(i);
     }
-    
-    return ans;
-    
+    return answer;
   }
 
-  
   void insert(Node parent, Node child){
-    if(child.x < parent.x)  {
+    if(child.x < parent.x) {
       if(parent.left == null){
         parent.left = child;
       }else{
@@ -65,25 +62,25 @@ class Solution {
       }
     }else {
       if(parent.right == null){
-        parent.right = child  ;
-      }else {
+        parent.right = child;
+      }else{
         insert(parent.right, child);
       }
     }
   }
-  
-  
+
   void preorder(Node node){
-    if (node == null) return;
-    preorderList.add(node.num);
+    if(node == null) return;
+    preorderResult.add(node.nodeNum);
     preorder(node.left);
     preorder(node.right);
   }
-  
+
   void postorder(Node node){
     if(node == null) return;
+
     postorder(node.left);
     postorder(node.right);
-    postorderList.add(node.num);
+    postorderResult.add(node.nodeNum);
   }
 }
