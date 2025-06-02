e927356ceb084edd8c2d8f5ca14d81a945dc6f68
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-06-02 11:28:58 +0900

    [level 3] Title: 양과 늑대, Time: 4.51 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,59 +1,44 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 2진 트리 모양 초원
- * 루트에서 시작하여 양을 모으려 한다 -
- *   - 각 노드를 방문할때, 해당 노드에 있던 양/늑대가 따라온다
- *   - 내가 모은 양의 수보다 늑대가 많거나 같다면, 양을 잡아먹는다
- *   - 최대한 많은 수의 양을 모아서 다시 루트 노드로
- */
 
+public class Solution {
 
-class Solution {
-
-  int maxSheepCount;
   List<List<Integer>> nodes = new ArrayList<>();
-
+  int maxSheepCount = 0;
   public int solution(int[] info, int[][] edges) {
-
-    for(int i = 0 ; i<info.length; i++){
+    for(int i = 0; i<info.length; i++){
       nodes.add(new ArrayList<>());
     }
 
-    for(int i = 0 ; i<edges.length; i++){
+    for(int i = 0; i<edges.length; i++){
       nodes.get(edges[i][0]).add(edges[i][1]);
     }
 
     List<Integer> toSearch = new ArrayList<>();
     toSearch.add(0);
-    search(info, toSearch, 0, 0, 0);
+    search(toSearch, info, 0,0,0);
     
     return maxSheepCount;
   }
-
-  void search(int[] info, List<Integer> toSearch, int current, int sheepCount, int wolfCount){
-    if(info[current] == 0) sheepCount++;
-    else wolfCount++;
-
-    if(sheepCount <= wolfCount) return;
-
-    maxSheepCount = Math.max(sheepCount, maxSheepCount);
+  
+  void search(List<Integer> toSearch, int[] info, int current, int wolf, int sheep){
+    if(info[current] == 0) sheep++;
+    else wolf++;
+    
+    if(wolf >= sheep) return;
+    maxSheepCount = Math.max(maxSheepCount, sheep);
     
     toSearch.addAll(nodes.get(current));
     toSearch.remove(Integer.valueOf(current));
     
-    for (int i = 0 ; i<toSearch.size(); i++){
-      
+    for(int i = 0; i<toSearch.size(); i++){
       int next = toSearch.get(i);
       List<Integer> newToSearch = new ArrayList<>(toSearch);
-      search(info, newToSearch, next, sheepCount, wolfCount);
-      
+      search(newToSearch, info, next, wolf, sheep);
     }
-
   }
-
-
 }
