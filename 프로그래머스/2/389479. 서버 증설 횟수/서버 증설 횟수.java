630f90c5d5b25f7387b516fea314a56408098e8b
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-08-01 14:10:21 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.97 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,51 +1,31 @@
-import java.util.*;
-/**
- * 같은 시간에 게임을 이용하는 사람이 m 명 늘어날 떄마다, 서버 1대 추가 필요.
- *
- * 어느 시간대의 이용자가 m명 미만이라면 증설 x
- *
- * 어느 시간대의 이용자가 n x m <= 이용자 수 < (n + 1) x m 이라면, 최소 n 대의 서버가 운영중이어야 한다
- *
- * 한번 증설한 서버는 k 시간동안 운영한다
- *       ex ) k = 5 일때 10시에 증설한 서버는 10 ~ 15 시 동안 운영
- *
- * 하루동안 모든 이용자가 게임을 하기 위해 서버를 최소 몇번 증설해야 하는지
- */
+import java.util.LinkedList;
+import java.util.Queue;
 
-class P{
-  int num, endTime;
-
-  public P(int num, int endTime) {
-    this.num = num;
-    this.endTime = endTime;
-  }
-}
 class Solution {
   public int solution(int[] players, int m, int k) {
     int answer = 0;
-    int currentServerCnt = 0;
-    Queue<P> q = new LinkedList<>();
 
-    for(int i = 0; i < players.length; i++){
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < players.length; i++) {
 
-      int currentPlayers = players[i];
-
-      while(!q.isEmpty() && q.peek().endTime <= i){
-        currentServerCnt -= q.poll().num;
+      while(!q.isEmpty() && q.peek() <= i){
+        q.poll();
       }
 
-      if(currentPlayers / m > currentServerCnt){
-        int serversToAdd = (currentPlayers / m) - currentServerCnt;
-        currentServerCnt += serversToAdd;
-        q.add(new P(serversToAdd, i + k));
-        answer += serversToAdd;
+
+      int current = players[i];
+      int currentServer = q.size();
+      int neededServer = current / m;
+
+      if(neededServer > currentServer){
+        for(int j = 0; j < neededServer - currentServer; j++){
+          q.add(i + k);
+          answer++;
+        }
       }
 
     }
 
-
     return answer;
   }
-
-
 }
