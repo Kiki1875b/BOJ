09ef8c3fce3f3daf5c8f4ef2f5ec0a25abe6bb6a
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-08-17 16:48:53 +0900

    [Gold IV] Title: 정복자, Time: 328 ms, Memory: 31340 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.java
@@ -0,0 +1,86 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class Edge implements Comparable<Edge>{
+
+    int a, b, cost;
+
+    public Edge(int a, int b, int cost) {
+        this.a = a;
+        this.b = b;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o){
+        return cost - o.cost;
+    }
+}
+class Main {
+    static int N, M, T;
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        T = Integer.parseInt(input[2]);
+
+        parent = new int[N + 1];
+        for(int i = 0; i<parent.length; i++) parent[i] = i;
+
+        List<Edge> edges = new ArrayList<>();
+
+        for(int i = 0; i<M; i++){
+            String[] tmp = br.readLine().split(" ");
+
+            int a = Integer.parseInt(tmp[0]);
+            int b = Integer.parseInt(tmp[1]);
+            int cost = Integer.parseInt(tmp[2]);
+
+            edges.add(new Edge(a, b, cost));
+        }
+
+        Collections.sort(edges);
+
+
+        int ans = 0; int cnt = 0;
+        for(Edge e : edges){
+            if(union(e.a, e.b)){
+                ans += (e.cost + T*cnt);
+                cnt++;
+                if(cnt == M - 1) break;
+            }
+        }
+
+        System.out.println(ans  );
+
+    }
+
+    static int find(int a){
+        if(a== parent[a])return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    static boolean union(int a, int b){
+        int pa = find(a);
+        int pb= find(b);
+
+        if(pa == pb) return false;
+        parent[pb] = pa;
+        return true;
+    }
+
+
+
+}
+
+
+
