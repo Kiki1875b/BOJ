d6174dbe582c40a36a47e662f41d47fd6976f7eb
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-10-06 16:06:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 41.00 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,49 +1,53 @@
+
 import java.util.*;
 
-/**
- * 길이가 같은 큐 2개
- * 하나에서 뽑아 다른 하나에 삽입할 수 있다
- * 목표 : 두 큐의 합이 같게 만들 수 있는 최소 횟수
- */
 class Solution {
-  public int solution(int[] queue1, int[] queue2) {
-    int answer = 0;
-    int n = queue1.length;
-    List<Integer> merged = new ArrayList<>();
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;        
+        long target = 0L;
+        long q1Sum = 0L;
+        long q2Sum = 0L;
+        
+        long[] q = new long[(queue1.length + queue2.length) * 2];
+        
+        for(int i = 0 ; i<queue1.length; i++){
+            q1Sum += (long) queue1[i];
+            q2Sum += (long) queue2[i];
+        }
+        
 
-    Long q1 = 0L;
-    Long q2 = 0L;
-    
-    for(int i : queue1) {
-      q1 += i;
-      merged.add(i);
+        target = (q1Sum + q2Sum) / 2;
+        if((q1Sum + q2Sum) % 2 != 0) return -1;
+        for(int i = 0; i<queue1.length; i++) q[i] = (long) queue1[i];
+        for(int i = 0; i<queue2.length; i++) q[i + queue1.length] = (long) queue2[i];
+        for(int i = 0; i<queue1.length + queue2.length; i++) q[i + queue1.length + queue2.length] = q[i];
+
+        int round = 0; boolean found = false;
+        int limit = queue1.length + queue2.length;
+        long curSum = q1Sum;
+        int left = 0; int right = queue1.length;
+        
+        // for(long i: q) System.out.print(i + " ");
+        // System.out.println();
+        // System.out.println("curSUm: " + curSum);
+        
+        while(round < limit* 2){
+            if(curSum == target){
+                found = true;
+                break;
+            }    
+            
+            if(curSum > target){
+                curSum -= q[left]; left++;
+                // System.out.println("subtract left - res: " + curSum);
+            } else {
+                curSum += q[right]; right++;
+                // System.out.println("add right - res: " + curSum);
+
+            }
+            round++;
+        }
+        
+        return found ? round : -1;
     }
-    for(int i : queue2) {
-      q2 += i;
-      merged.add(i);
-    }
-
-    if((q1 + q2) % 2 != 0 ) return -1;
-
-    int left = 0;
-    int right = n;
-
-    while(left <= right && right < merged.size()){
-      if(q1.equals(q2)) return answer;
-
-      if(q1 < q2){
-        q1 += merged.get(right);
-        q2 -= merged.get(right);
-        right++;
-      }else{
-        q1 -= merged.get(left);
-        q2 += merged.get(left);
-        left++;
-      }
-      answer++;
-    }
-
-    return -1;
-  }
-  
 }
