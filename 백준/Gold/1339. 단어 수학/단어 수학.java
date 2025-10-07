2201138f134f0104224f278414b9fe49dbc43275
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-10-07 14:26:07 +0900

    [Gold IV] Title: 단어 수학, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,78 +1,71 @@
+
 import java.util.*;
 import java.io.*;
 
-
-class CharacterOccurance implements Comparable<CharacterOccurance> {
-    
+class CO implements Comparable<CO>{
     char c;
     int weight;
-    
-    public CharacterOccurance(char c, int weight){
+
+    public CO(char c, int weight) {
         this.c = c;
-        this.weight = weight;
+        this.weight= weight;
     }
-    
+
     @Override
-    public int compareTo(CharacterOccurance co){
-        return co.weight - this.weight; // 큰 가중치 먼저
+    public int compareTo(CO o){
+        return o.weight - this.weight;
     }
 }
-public class Main {
-    
-    static Map<Character, Integer> m = new HashMap<>();
-    
-    public static void main(String[] args) throws IOException{
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<String> l = new ArrayList<>();
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        for(int i = 0 ;i <N; i++){
-            l.add(br.readLine());
-        }
-        
-        PriorityQueue<CharacterOccurance> pq = new PriorityQueue<>();
 
-        for(String s : l){
-            for(int i = 0; i<s.length(); i++){
-                char c = s.charAt(i);
-                int idx = (int) Math.pow(10, s.length() - i - 1);
-                int cur = m.getOrDefault(c, 0);
-                cur += idx;   // 가중치 누적
-                m.put(c, cur);
+
+public class Main {
+
+    static Map<Character, Integer> m = new HashMap<>();
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Character, Integer> m = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+        List<String> l = new ArrayList<>();
+
+        for(int i = 0; i<N; i++){
+            String tmp = br.readLine();
+            l.add(tmp);
+            for(int j = 0; j<tmp.length(); j++){
+                char c = tmp.charAt(j);
+                int w = m.getOrDefault(c, 0);
+                int toAdd = (int) Math.pow(10, tmp.length() - j - 1);
+                w += toAdd;
+                m.put(c, w);
             }
         }
-        
-        
-        for(Character c : m.keySet()){
-            int cur = m.get(c);
-            // System.out.println("CHAR: " + c + " maxIdx: " + cur[0] + " times: " + cur[1]);
-            pq.add(new CharacterOccurance(c, cur));
+
+        PriorityQueue<CO> pq = new PriorityQueue<>();
+
+        for(Character key: m.keySet()){
+            pq.add(new CO(key, m.get(key)));
         }
-        
+
         int num = 9;
-        
-        Map<Character, Integer> cVal = new HashMap<>();
-        
-        while(!pq.isEmpty()){
-            CharacterOccurance co = pq.poll();
-            cVal.put(co.c, num);
-            // System.out.println("CHAR: " + co.c + " val: " + num);
+        Map<Character, Integer> vMap = new HashMap<>();
+
+        while (!pq.isEmpty()){
+            CO cur = pq.poll();
+            vMap.put(cur.c, num);
             num--;
         }
-        
         int ans = 0;
         for(String s : l){
             String tmp = "";
             for(int i = 0; i<s.length(); i++){
-                int val = cVal.get(s.charAt(i));
+                int val = vMap.get(s.charAt(i));
                 tmp += Integer.toString(val);
-                
             }
             ans += Integer.parseInt(tmp);
         }
-        
+
         System.out.println(ans);
     }
 }
