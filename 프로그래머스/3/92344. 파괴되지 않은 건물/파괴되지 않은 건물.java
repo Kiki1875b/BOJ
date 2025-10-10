65fea835783e5e0556a92476b9a812fa91238282
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-10-10 12:23:17 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 41.67 ms, Memory: 209 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,45 +1,42 @@
-
-import java.util.*;
-
-
 class Solution {
-  int[][] prefixSum;
-  public int solution(int[][] board, int[][] skill) {
-    int answer = 0;
-        prefixSum = new int[board.length + 2][board[0].length + 2 ];
-
-    for(int[] s : skill){
-      int type = s[0];
-      int r1 = s[1];
-      int c1 = s[2];
-      int r2 = s[3];
-      int c2 = s[4];
-      int degree = type == 1 ? -s[5] : s[5];
-      
-      prefixSum[r1][c1] += degree;
-      prefixSum[r1][c2 + 1] -= degree;
-      prefixSum[r2 + 1][c1] -= degree;
-      prefixSum[r2 + 1][c2 + 1] += degree;
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        int N = board.length; int M = board[0].length;
+        
+        int[][] effect = new int[N + 1][M + 1];
+        
+        for(int[] s: skill){
+            int type = s[0];
+            int x1 = s[1], y1 = s[2], x2 = s[3], y2 = s[4], degree = s[5];
+            
+            if(type == 1) degree = -degree;
+            
+            effect[x1][y1] += degree;
+            effect[x2 + 1][y2 + 1] += degree;
+            effect[x1][y2 + 1] -= degree;
+            effect[x2 + 1][y1] -= degree;
+        }
+        
+        for(int i = 0; i<= N; i++){
+            for(int j = 1; j<= M; j++){
+                effect[i][j] += effect[i][j - 1];
+            }
+        }
+        
+        for(int i = 1; i <= N; i++){
+            for(int j = 0; j<= M; j++){
+                effect[i][j] += effect[i - 1][j];
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0 ; i<N; i++){
+            for(int j = 0; j<M; j++){
+                if(board[i][j] + effect[i][j] > 0) ans++;
+            }
+        }
+        
+        
+        return ans;
     }
-    
-    
-    for(int i = 0; i<prefixSum.length; i++){
-      for (int j = 1; j<prefixSum[0].length; j++){
-        prefixSum[i][j] += prefixSum[i][j - 1];
-      }
-    }
-    
-    for(int i = 1; i<prefixSum.length; i++){
-      for(int j = 0; j<prefixSum[0].length; j++){
-        prefixSum[i][j] += prefixSum[i - 1][j];
-      }
-    }
-    
-    for(int i = 0; i<board.length; i++){
-      for(int j = 0; j<board[0].length; j++){
-        if(board[i][j] + prefixSum[i][j] > 0) answer++;
-      }
-    }
-    return answer;
-  }
 }
