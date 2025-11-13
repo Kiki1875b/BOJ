d39fe9ad29be791d8ba3cb49185295b1cf7efb62
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-11-13 21:26:59 +0900

    [Gold IV] Title: 동전 1, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,32 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 class Main {
     
     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
+
         
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
         
-        int[] coins = new int[n];
-        
-        for(int i = 0 ; i<n; i++){
+        int[] coins = new int[N];
+        for(int i = 0; i<N; i++){
             coins[i] = Integer.parseInt(br.readLine());
         }
         
-        int[] dp = new int[k + 2]; // dp[i] = i원을 만들 수 있는 경우의 수
+        int[] dp = new int[K + 1]; // dp[i] = i 원을 만들 수 있는 경우의 수
+        Arrays.fill(dp, 0);
         dp[0] = 1;
-        for(int coin: coins){
-            for(int x = coin; x <= k; x++){
-                dp[x] += dp[x - coin];
+        
+        for(int i = 0; i<N; i++){
+            int coin = coins[i];
+            
+            for(int j = coin; j<= K; j++){
+                 dp[j] += dp[j - coin];   
             }
         }
-        
-        System.out.println(dp[k]);
+        System.out.println(dp[K]);        
     }
 }
