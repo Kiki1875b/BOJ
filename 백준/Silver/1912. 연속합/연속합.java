6a286fbf3df55f1d8f939074a908eda71799bbd9
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-11-14 13:16:04 +0900

    [Silver II] Title: 연속합, Time: 244 ms, Memory: 24796 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,37 +1,33 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
+class Main {
     
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
+ 
+        int n = Integer.parseInt(br.readLine());
         
-        long[] seq = new long[input.length];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        
-        
-        for(int i = 0 ; i<input.length; i++){
-            seq[i] = Long.parseLong(input[i]);
+        long[] arr = new long[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = Long.parseLong(st.nextToken());
         }
         
-        if(N == 1) {
-            System.out.println(seq[0]);
-            return;
+        if(n == 1){
+            System.out.println(arr[0]); return;
         }
         
         
-        long[] dp = new long[seq.length];
-        
-        dp[0] = seq[0];
-        long ans = seq[0];
-        for(int i = 1; i<seq.length; i++){
-            dp[i] = Math.max(dp[i - 1] + seq[i], seq[i]);
+        long[] dp = new long[arr.length];
+        dp[0] = arr[0];
+        long ans = arr[0];
+        for(int i = 1; i<arr.length; i++){
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
             ans = Math.max(dp[i], ans);
         }
-        
-        
+
         System.out.println(ans);
     }
 }
