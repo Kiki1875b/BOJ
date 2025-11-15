15792fe5c6b2b98c95a9111c2c4cf9fa8717d3f8
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-11-15 19:13:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 124 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,36 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-class Main {
-    
-    static int N, K;
-    
+class Main{
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+    
         String[] input = br.readLine().split(" ");
         
-        N = Integer.parseInt(input[0]); K = Integer.parseInt(input[1]);
-        
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
         
         int[][] items = new int[N][2];
-        
-        for(int i = 0 ; i< N; i++){
+        for(int i = 0; i < N; i++){
             String[] tmp = br.readLine().split(" ");
-            
             items[i][0] = Integer.parseInt(tmp[0]);
             items[i][1] = Integer.parseInt(tmp[1]);
         }
         
-        int[] dp = new int[K + 1]; // dp[i] = i 무개일때 최대 가치
+        int[] dp = new int[K + 1]; // dp[i] = i 무개 까지 최대 가치
+        dp[0] = 0;
         
         for(int i = 0; i<N; i++){
             int weight = items[i][0];
-            int value = items[i][1];
+            int val = items[i][1];
             
             for(int j = K; j >= weight; j--){
-                dp[j] = Math.max(dp[j], dp[j - weight] + value);
+                dp[j] = Math.max(dp[j], dp[j - weight] + val);
             }
         }
         
