358fd501fbac7f7cf8997bb4af4a62dd4f545b6e
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-12-11 20:59:38 +0900

    [Gold IV] Title: 강의실 배정, Time: 700 ms, Memory: 83800 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -5,31 +5,38 @@ class Main {
     
     public static void main(String[] args) throws IOException {
         
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        List<int[]> events = new ArrayList<>();
+        
+        int[][] arr = new int[N][2];
         
         for(int i = 0; i<N; i++){
             String[] input = br.readLine().split(" ");
             
-            events.add(
-                new int[]{Integer.parseInt(input[0]), 1}
-            );
-            
-            events.add(
-                new int[]{Integer.parseInt(input[1]), -1}
-            );
+            arr[i][0] = Integer.parseInt(input[0]);
+            arr[i][1] = Integer.parseInt(input[1]);
         }
         
-        Collections.sort(events, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-    
-        int ans = 0; int cur =0;
-        for(int[] n : events){
-            cur += n[1];
-            ans = Math.max(cur, ans);
+        Arrays.sort(arr, (a,b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+        
+        
+        int ans = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(int i = 0; i<N; i++){
+            int[] cur = arr[i];
+            int start = cur[0]; int end = cur[1];
+            
+            if(!q.isEmpty() && q.peek() <= start){
+                q.poll();
+            }
+            
+            q.add(end);
+            ans = Math.max(ans, q.size());
         }
         
         System.out.println(ans);
