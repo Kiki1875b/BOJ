c7fcd4ae24a05a3958516c852fd19eadcd1d04d4
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2025-12-24 17:00:17 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,37 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 class Main {
     
     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-         String[] input = br.readLine().split(" ");
+        String[] input = br.readLine().split(" ");
         
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
         
-        int[] coins = new int[n];
-        int[] dp = new int[k + 1];
+        int[] coins = new int[N];
         
-        for(int i = 0 ; i< coins.length; i++){
+        for(int i = 0; i<N; i++){
             coins[i] = Integer.parseInt(br.readLine());
         }
         
+        int[] dp = new int[K + 1]; // dp[i] = k원을 만들 수 있는 최소 동전 수
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i <= k; i++) dp[i] = Integer.MAX_VALUE;
         
-        for(int coin : coins){
-            for(int i = coin; i<=k; i++){
-                if(dp[i - coin] != Integer.MAX_VALUE){
-                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+        for(int i = 0; i<N; i++){
+            int coin = coins[i];
+            
+            for(int j = coin; j <= K ; j++){
+                if(dp[j - coin] != Integer.MAX_VALUE){
+                    dp[j] = Math.min(dp[j], dp[j - coin] + 1);
                 }
             }
         }
         
-        int ans = dp[k] == Integer.MAX_VALUE ? -1 : dp[k];
-        
-        System.out.println(ans);
+        System.out.println(dp[K] == Integer.MAX_VALUE ? -1 : dp[K]);
     }
 }
