8023dbcbd931a90f0f629382caac0488f442b40e
Kiki1875b <92781767+Kiki1875b@users.noreply.github.com>
2026-01-05 13:44:34 +0900

    [Platinum IV] Title: 도미노, Time: 912 ms, Memory: 185280 KB -BaekjoonHub

--- 백준/Platinum/4196. 도미노/도미노.java
+++ 백준/Platinum/4196. 도미노/도미노.java
@@ -2,43 +2,45 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    
     static int[] disc;
     static int[] low;
     static boolean[] visited;
     static boolean[] inStack;
-    static int[] sccId;
-    static int sccCnt =0;
-    static int[] indegree;
-    static Stack<Integer> s;
+    static Stack<Integer> st;
     static List<List<Integer>> graph;
+    static int[] sccId;
     static int time = 0;
+    static int sccCnt = 0;
+    static int[] indegree;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for(int t = 0; t< T; t++){
-            String[] ss = br.readLine().split(" ");
-            int N = Integer.parseInt(ss[0]);
-            int M = Integer.parseInt(ss[1]);
+        int t = Integer.parseInt(br.readLine());
+        
+        for(int T = 0; T < t; T++){
+            String[] s = br.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
             
             disc = new int[N + 1];
             low = new int[N + 1];
-            indegree = new int[N + 1];
             visited = new boolean[N + 1];
             inStack = new boolean[N + 1];
-            sccId = new int[N + 1];
+            st = new Stack<>();
             graph = new ArrayList<>();
-            s = new Stack<>();
-            sccCnt =0;
+            sccId = new int[N + 1];
             time = 0;
+            sccCnt = 0;
+            indegree = new int[N + 1];
             
             for(int i = 0; i<=N; i++) graph.add(new ArrayList<>());
-            
-            for(int i = 0 ;i<M; i++){
-                ss = br.readLine().split(" ");
-                int a = Integer.parseInt(ss[0]);
-                int b = Integer.parseInt(ss[1]);
-                
+            for(int i = 0; i<M; i++){
+                s = br.readLine().split(" ");
+                int a = Integer.parseInt(s[0]);
+                int b = Integer.parseInt(s[1]);
                 graph.get(a).add(b);
+                
             }
             
             for(int i = 1; i<=N; i++){
@@ -53,43 +55,46 @@ class Main {
                 }
             }
             
-            int answer = 0;
-            for(int i = 0; i < sccCnt; i++){
-                if(indegree[i] == 0) answer++;
+            int ans = 0;
+            for(int i = 0; i< sccCnt; i++){
+                if(indegree[i] == 0){
+                    ans++;
+                }
             }
-
-            System.out.println(answer);
+            
+            System.out.println(ans);
+            
         }
+        
+        
     }
     
     
     static void dfs(int node){
-        
         visited[node] = true;
-        disc[node] = low[node] = time++;
-        s.push(node);
         inStack[node] = true;
+        st.push(node);
+        disc[node] = low[node] = time++;
         
-        for(int next :graph.get(node)){
+        for(int next : graph.get(node)){
             if(!visited[next]){
                 dfs(next);
                 low[node] = Math.min(low[node], low[next]);
+                
             }else if(inStack[next]){
                 low[node] = Math.min(low[node], disc[next]);
             }
         }
         
+        
         if(low[node] == disc[node]){
-            while(true) {
-                int x = s.pop();
+            while(true){
+                int x = st.pop();
                 inStack[x] = false;
                 sccId[x] = sccCnt;
                 if(x == node) break;
             }
             sccCnt++;
         }
-        
-        
-        
     }
 }
